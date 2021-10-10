fbbd02b02f6c84b02c496b152d64833b7ff5ea63
Bryan <bryanjguilas@gmail.com>
2021-10-10 14:14:44 -0700

    commit

--- controllers/show.js
+++ controllers/show.js
@@ -1,15 +1,15 @@
-import Application from '../models/Application.js';
-import catchAsync from '../utils/catchAsync.js';
-
-const showALlApplication = catchAsync(async (req, res) => {
-	try {
-		const applications = await Application.find();
-		res.json(applications);
-	} catch (err) {
-		console.error(err.message);
-		res.status(400).json({msg: 'Unable to retrieve applications.'});
-	}
-});
-
-export default showALlApplication;
+// import Application from '../models/Application.js';
+// import catchAsync from '../utils/catchAsync.js';
+//
+// const showALlApplication = catchAsync(async (req, res) => {
+// 	try {
+// 		const applications = await Application.find();
+// 		res.json(applications);
+// 	} catch (err) {
+// 		console.error(err.message);
+// 		res.status(400).json({msg: 'Unable to retrieve applications.'});
+// 	}
+// });
+//
+// export default showALlApplication;
 
